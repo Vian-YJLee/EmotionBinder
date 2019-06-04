c7687dbe474e2913b6bad2e390955dcad2732b73
Vian-YJLee <Vianney.yjlee@gmail.com>
2019-06-04 17:41:52 +0900

    Initial Commit

---
+++ EmotionBinder/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  EmotionBinder
+//
+//  Created by LeeYongJin on 04/06/2019.
+//  Copyright © 2019 vian. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EmotionBinder/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  EmotionBinder
+//
+//  Created by LeeYongJin on 04/06/2019.
+//  Copyright © 2019 vian. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
